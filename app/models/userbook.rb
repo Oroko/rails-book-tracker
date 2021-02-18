b7ab58a320fbb327082e4c9ed1b8b2ac1cb6e029
Brian Franklin <brianfranklinmusic@gmail.com>
2021-02-17 22:24:59 -0500

    added seed data and index view of books

--- app/models/userbook.rb
+++ app/models/userbook.rb
@@ -1,5 +1,4 @@
 class Userbook < ApplicationRecord
   belongs_to :user
   belongs_to :book
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+book = Book.new
+book.title = "The Alchemist"
+book.author = "Paulo Coelho"
+book.pages = 233
+book.genre = "Adventure"
+book.status = "read"
+book.save
+
+book2 = Book.new
+book2.title = "Good Omens"
+book2.author = "Neil Gaiman"
+book2.pages = 200
+book2.genre = "Comedy"
+book2.status = "read"
+book2.save
+
+book3 = Book.new 
+book3.title = "Slaughterhouse-Five"
+book3.author = "Kurt Vonnegut"
+book3.pages = 250
+book3.genre = "Science Fiction"
+book3.status = "read"
+book3.save
